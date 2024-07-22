ff7e63a09aef4c8695c2286c6e5bed0b38e4a012
Valeentin87 <valeentin87@list.ru>
2024-07-22 18:07:09 +0300

    Функционал реализованю Задание выполнено

--- NodeJS_sem1/http-server1.js
+++ NodeJS_sem1/http-server1.js
@@ -8,15 +8,21 @@
 // должно увеличиваться на единицу каждый раз, когда загружается страница
 
 const http = require('http');
+
+let count1 = 0;
+let count2 = 0;
 const server = http.createServer((req, res) => {
+
     console.log("Запрос получен");
     if (req.url === '/') {
+        count1 += 1;
         res.writeHead(200, {'Content-Type':  'text/html; charset=UTF-8'});
-        res.end('<h1> Добро пожаловать !!! Ниже ссылка на вторую страницу</h1> <a href="http://127.0.0.1:3000/about">About</a>');                
+        res.end(`<h1> Добро пожаловать !!! Ниже ссылка на вторую страницу Cчётчик равен ${count1}</h1> <a href="http://127.0.0.1:3000/about">About</a>`);                
     }
     else if (req.url === '/about') {
+        count2 += 1;
         res.writeHead(200, {'Content-Type':  'text/html; charset=UTF-8'});
-        res.end('<h1> Добро пожаловать !!! Ниже ссылка на первую страницу</h1> <a href="http://127.0.0.1:3000/">Main</a>');                
+        res.end(`<h1> Добро пожаловать !!! Ниже ссылка на первую страницу. Cчётчик равен ${count2}</h1> <a href="http://127.0.0.1:3000/">Main</a>`);                
     }
     else {
         res.writeHead(404, {'Content-Type':  'text/html; charset=UTF-8'});
